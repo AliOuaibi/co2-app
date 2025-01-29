5451ffbaaa80b3c7ebdabf1b2a70109188e99dae
Ali <ali.ouaibi@epitech.eu>
2025-01-29 17:04:52 +0100

    create hooks useIsmobile

--- src/pages/home/SliderSection.js
+++ src/pages/home/SliderSection.js
@@ -1,6 +1,9 @@
 import sliderImg1 from "../../assets/images/slider/banner.jpg";
+import useIsMobile from "../../hooks/useIsMobile";
 
 const HomeSlider = () => {
+  const isMobile = useIsMobile();
+
   return (
     <>
       <div className="react-slider-part">
@@ -8,14 +11,20 @@ const HomeSlider = () => {
             <div className="single-slide">
               <div className="slider-img">
                 <img className="desktop" src={sliderImg1} alt="banniere du site"/>
+                <img className="mobile" src={sliderImg1} alt="banniere du site"/>
               </div>
               <div className="container">
                 <div className="slider-content">
                   <div className="content-part">
-                    <span className="slider-pretitle wow animate__fadeInUp"
-                          data-wow-duration="1s">Votre atout Formation</span>
-                    <h3 className="slider-title wow animate__fadeInUp" data-wow-duration="1s" style={{ color: '#8c52ff', WebkitTextStroke: '2px white', fontSize: "250%"}}>
-                      Retrouvez toutes nos solutions et formations adaptées à vos besoins et vos envies.<br/>
+                    <span className="slider-pretitle wow animate__fadeInUp" data-wow-duration="1s" style={{fontSize: isMobile ? "50%": "250%"}}>
+                      Votre atout Formation
+                    </span>
+                    <h3
+                      className="slider-title wow animate__fadeInUp"
+                      data-wow-duration="1s"
+                      style={{ color: `${isMobile ? "#000" : "#8c52ff"} `, WebkitTextStroke: `${isMobile ? "0.5px" : "2px"} white`, fontSize: isMobile ? "110%": "250%"}}
+                    >
+                      Retrouvez toutes nos solutions et formations adaptées à vos besoins et vos envies.{isMobile ? <br/> : ""}
                       Formations au catalogue ou sur-mesure
                     </h3>
                   </div>
